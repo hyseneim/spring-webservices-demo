fe8eb7de080a5a0fc9c564bd93c59a8760cb6e74
Gabriele Cipolloni <gabri9501@gmail.com>
2017-05-30 10:16:04 +0200

    First commit.

---
+++ src/main/java/io/spring/guides/gs_producing_web_service/package-info.java
@@ -0,0 +1,9 @@
+//
+// Questo file è stato generato dall'architettura JavaTM per XML Binding (JAXB) Reference Implementation, v2.2.7 
+// Vedere <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
+// Generato il: 2017.05.30 alle 10:08:15 AM CEST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://spring.io/guides/gs-producing-web-service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package io.spring.guides.gs_producing_web_service;
---
+++ src/main/java/it/keybiz/spring/webservices/demo/SpringWebservicesDemoApplication.java
@@ -0,0 +1,12 @@
+package it.keybiz.spring.webservices.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebservicesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebservicesDemoApplication.class, args);
+	}
+}
